fd9a0542f32ab864f33a5569db1c014fd5df55d0
Robson Nascimento <Robson.Do.Nascimento@vanderlande.com>
2022-10-10 16:03:39 +0200

    test precomit

---
+++ play.sh
@@ -0,0 +1,2 @@
+echo "/c/Users/nlrdn/GravitySource/rluisn-devtest/versioning/.git/hooks" | awk '{ gsub(/.git/, ""); print }' | awk '{ gsub(/hooks/, ""); print }' | sed 's/.$//'
+#awk '{gsub(/[^[:alnum:]^[:blank:]]*/,x); print}'
